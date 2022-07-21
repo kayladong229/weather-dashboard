1f436360a2038da58f9f8a6f488d2e64e81afb0f
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-07-20 19:22:34 -0700

    Created Assets folder and moved js and css files there, also wrote initial html draft

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+// Declare variables
+var cityListEl = $('#city-list');
+var currentWeather = $('#current-weather');
+var fiveDayForecast = $('#five-day-forecast');
+
+document.getElementById("search").on("click", function(){
+    if (document.getElementById("search-field") !== null) {
+        
+    }
+}
